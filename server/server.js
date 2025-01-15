927867049c842b3c6fd28e82db33c34159c2664f
juandavidporrasgomez9 <juandavidporrasgomez9@gmail.com>
2025-01-15 15:17:02 -0500

    Inicio del proyecto

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/usuario', function (req, res) {
+  res.json('GET usuario');
+})
+
+app.post('/usuario', function (req, res) {
+
+  let body = req.body;
+
+  if (body.nombre === undefined) {
+    res.status(400).json({
+      ok: false,
+      message: 'Se requiere el parametro nombre',
+    });
+  } else {
+    res.json({
+      persona: body
+    });
+  }
+})
+
+app.put('/usuario/:id', function (req, res) {
+
+  let id = req.params.id;
+  res.json({ id });
+
+})
+
+app.delete('/usuario', function (req, res) {
+  res.json('DELETE usuario');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando en el puerto ', process.env.PORT);
+})
